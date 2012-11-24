819de98a18d87018aee905bf1e6afab6d3f8197c
David Fischer <djfische@gmail.com>
2012-11-23 18:39:36 -0800

    Added test for file-like obj

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,7 +1,10 @@
 import unittest
+from StringIO import StringIO
+
 from reqfileparser import parse
 
 class TestParser(unittest.TestCase):
     def test_empty(self):
         self.assertEqual(parse(''), [])
+        self.assertEqual(parse(StringIO('')), [])
     
