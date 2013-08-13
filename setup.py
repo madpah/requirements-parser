3058539596fe8fe851aae79e396a1223996ccbd3
David Fischer <djfische@gmail.com>
2013-08-12 21:25:36 -0700

    Refactored tests into test files
    Each test has a txt file and an expected file (except "fails")

--- setup.py
+++ setup.py
@@ -5,20 +5,27 @@ except ImportError:
     import warnings
     warnings('Please install setuptools or distribute!')
 
+# Workaround for http://bugs.python.org/issue15881#msg170215
+try:
+    import multiprocessing  # noqa
+except ImportError:
+    pass
+
+
 long_description = open('README.rst').read()
 
 setup(
-    name = 'requirements-parser',
-    version = '0.0.4',
-    description = 'Parses Pip requirement files',
-    long_description = long_description,
-    author = 'David Fischer',
-    author_email = 'djfische@gmail.com',
-    url = 'https://github.com/davidfischer/requirements-parser',
-    license = 'BSD',
-    platforms = ['OS Independent'],
-    packages = ['requirements'],
-    classifiers = [
+    name='requirements-parser',
+    version='0.0.4',
+    description='Parses Pip requirement files',
+    long_description=long_description,
+    author='David Fischer',
+    author_email='djfische@gmail.com',
+    url='https://github.com/davidfischer/requirements-parser',
+    license='BSD',
+    platforms=['OS Independent'],
+    packages=['requirements'],
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
@@ -31,5 +38,6 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    test_suite='tests',
+    tests_require=['nose'],
+    test_suite='nose.collector',
 )
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,147 +1,56 @@
-import os.path
-import unittest
+import os
+import json
+from nose.tools import raises, assert_equal
 import warnings
-
-try:
-    # Python 2.x compatibility
-    from StringIO import StringIO
-except ImportError:
-    # Python 3.x only
-    from io import StringIO
 
 from requirements import parse
 
-this_dir = os.path.dirname(__file__)
-
-# useful for testing purposes
-listparse = lambda x: list(parse(x))
+REQFILE_DIR = os.path.join(os.path.dirname(__file__), 'reqfiles')
 
 
-class TestParser(unittest.TestCase):
-    def test_empty(self):
-        self.assertEqual(listparse(''), [])
-        self.assertEqual(listparse(StringIO('')), [])
+def listify(iterable):
+    out = []
+    for item in iterable:
+        if isinstance(item, dict):
+            for key in item:
+                if isinstance(item[key], tuple) or isinstance(item[key], list):
+                    item[key] = listify(item[key])
+        elif isinstance(item, tuple) or isinstance(item, list):
+            item = listify(item)
+        out.append(item)
+    return out
 
-    def test_comment(self):
-        self.assertEqual(listparse('#comment>>1.2.'), [])
 
-        # This gets handled even though it's against the spec
-        out = listparse('req==1.0    # comment')
-        self.assertEqual(out[0]['name'], 'req')
-        self.assertEqual(out[0]['specs'], [('==', '1.0')])
+def test_requirement_files():
+    for fn in os.listdir(REQFILE_DIR):
+        def fancy(f):
+            f.description = "%s.%s: %s" % (f.__module__, f.__name__, fn)
+            return f
 
-    def test_invalid(self):
-        # invalid operators
-        self.assertRaises(ValueError, listparse, 'test>>1.2.0')
-        self.assertRaises(ValueError, listparse, 'test=>1.2.0')
+        @fancy
+        @raises(ValueError)
+        def check_fail(s):
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                list(parse(s))
 
-        # invalid editables
-        self.assertRaises(ValueError, listparse, 'novcs+http://example.com#egg=a')
-        self.assertRaises(ValueError, listparse, 'svn+http://example.com#egg')
+        @fancy
+        def check(s, expected):
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                assert_equal(listify(parse(s)), expected)
 
-    def test_editable(self):
-        out = listparse('-e svn+svn://svn.myproject.org/svn/MyProject#egg=MyProject')
-        self.assertEqual(out[0]['vcs'], 'svn')
-        self.assertEqual(out[0]['name'], 'MyProject')
-        self.assertEqual(out[0]['uri'], 'svn://svn.myproject.org/svn/MyProject')
+        fp = os.path.join(REQFILE_DIR, fn)
 
-        out = listparse('--editable git://git.myproject.org/MyProject.git@da39a3ee5e6b4b0d3255bfef95601890afd80709#egg=MyProject')
-        self.assertEqual(out[0]['vcs'], None)
-        self.assertEqual(out[0]['name'], 'MyProject')
-        self.assertEqual(out[0]['uri'], 'git://git.myproject.org/MyProject.git@da39a3ee5e6b4b0d3255bfef95601890afd80709')
+        # skip ".expected" files
+        if not os.path.isfile(fp) or not fp.endswith('.txt'):
+            continue
 
-        out = listparse('--editable hg+http://hg.myproject.org/MyProject/@special_feature#egg=MyProject')
-        self.assertEqual(out[0]['vcs'], 'hg')
-        self.assertEqual(out[0]['name'], 'MyProject')
-        self.assertEqual(out[0]['uri'], 'http://hg.myproject.org/MyProject/@special_feature')
-
-        out = listparse('-e bzr+lp:MyProject#egg=MyProject')
-        self.assertEqual(out[0]['vcs'], 'bzr')
-        self.assertEqual(out[0]['name'], 'MyProject')
-        self.assertEqual(out[0]['uri'], 'lp:MyProject')
-
-    def test_file(self):
-        out = listparse('file:///path/to/your/lib/project#egg=MyProject')
-        self.assertEqual(out[0]['name'], 'MyProject')
-        self.assertEqual(out[0]['path'], 'file:///path/to/your/lib/project')
-
-        out = listparse('file:../../lib/project#egg=MyProject')
-        self.assertEqual(out[0]['name'], 'MyProject')
-        self.assertEqual(out[0]['path'], 'file:../../lib/project')
-
-    def test_normal(self):
-        out = listparse('MyPackage')
-        self.assertEqual(out[0]['name'], 'MyPackage')
-
-        out = listparse('Framework==0.9.4')
-        self.assertEqual(out[0]['name'], 'Framework')
-        self.assertEqual(out[0]['specs'][0], ('==', '0.9.4'),)
-
-        out = listparse('Library>=0.2')
-        self.assertEqual(out[0]['name'], 'Library')
-        self.assertEqual(out[0]['specs'][0], ('>=', '0.2'),)
-
-    def test_extras(self):
-        out = listparse('MyPackage[PDF]==3.0')
-        self.assertEqual(out[0]['name'], 'MyPackage')
-        self.assertEqual(out[0]['extras'], ['pdf'])
-        self.assertEqual(out[0]['specs'][0], ('==', '3.0'),)
-
-        out = listparse('Fizzy [foo, bar]')
-        self.assertEqual(out[0]['name'], 'Fizzy')
-        self.assertEqual(out[0]['extras'], ['foo', 'bar'])
-
-    def test_specs(self):
-        out = listparse('PickyThing<1.6,>1.9,!=1.9.6,<2.0a0,==2.4c1')
-        self.assertEqual(out[0]['name'], 'PickyThing')
-        self.assertEqual(len(out[0]['extras']), 0)
-        self.assertEqual(len(out[0]['specs']), 5)
-        self.assertEqual(out[0]['specs'], [
-            ('<', '1.6'),
-            ('>', '1.9'),
-            ('!=', '1.9.6'),
-            ('<', '2.0a0'),
-            ('==', '2.4c1'),
-        ])
-
-    def test_names(self):
-        # Checks '.' in names
-        out = listparse('z3c.checkversions==0.4.1')
-        self.assertEqual(out[0]['name'], 'z3c.checkversions')
-        self.assertEqual(out[0]['specs'], [('==', '0.4.1')])
-
-    def test_warnings(self):
-        with warnings.catch_warnings(record=True) as w:
-            warnings.simplefilter("always")
-            out = listparse('--requirement reqfile.txt')
-            self.assertEqual(len(w), 1)
-            self.assertEqual(out, [])
-
-    def test_fullfile(self):
-        with open(os.path.join(this_dir, 'reqfiles', 'rtfd_requirements.txt'), 'r') as f:
-            out = listparse(f)
-            self.assertEqual(len(out), 31)
-            self.assertEqual(out[0]['name'], 'Distutils2')
-            self.assertEqual(out[0]['specs'], [('==', '1.0a3')])
-            self.assertEqual(out[1]['name'], 'Sphinx')
-            self.assertEqual(out[1]['specs'], [('==', '1.1.2')])
-            self.assertEqual(out[24]['name'], 'django_haystack')
-            self.assertEqual(out[24]['vcs'], 'git')
-            self.assertEqual(out[24]['uri'], 'git://github.com/toastdriven/django-haystack@259274e4127f723d76b893c87a82777f9490b960')
-
-        with open(os.path.join(this_dir, 'reqfiles', 'rtfd_deploy_requirements.txt'), 'r') as f:
-            with warnings.catch_warnings(record=True) as w:
-                warnings.simplefilter("always")
-                out = listparse(f)
-                self.assertEqual(len(out), 5)
-                self.assertEqual(len(w), 1)  # warning for recursive reqs
-                self.assertEqual(out[0]['name'], 'psycopg2')
-                self.assertEqual(out[4]['name'], 'dnspython')
-
-        with open(os.path.join(this_dir, 'reqfiles', 'crateio_requirements.txt'), 'r') as f:
-            with warnings.catch_warnings(record=True) as w:
-                warnings.simplefilter("always")
-                out = listparse(f)
-                self.assertEqual(len(w), 1)  # warning for --extra-index-url
-                self.assertEqual(out[0]['name'], 'Babel')
-                self.assertEqual(out[0]['specs'], [('==', '0.9.6')])
+        with open(fp, 'r') as f:
+            txt = f.read()
+            if 'fail' in fn:
+                yield check_fail, txt
+            else:
+                with open(fp[:-4] + '.expected', 'r') as f2:
+                    expected = json.loads(f2.read())
+                    yield check, txt, expected
