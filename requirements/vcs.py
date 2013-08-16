a7a4910a78cc38881c7a6559a45d274f146ca3c7
David Fischer <djfische@gmail.com>
2013-08-15 19:56:24 -0700

    Big parser refactor
    * broke the parser into a Requirement class
    * refactored the unit test suite
    * better handling of VCS

---
+++ requirements/vcs.py
@@ -0,0 +1,28 @@
+VCS = [
+    'git',
+    'hg',
+    'svn',
+    'bzr',
+]
+
+VCS_SCHEMES = [
+    'git',
+    'git+https',
+    'git+ssh',
+    'git+git',
+    'hg+http',
+    'hg+https',
+    'hg+static-http',
+    'hg+ssh',
+    'svn',
+    'svn+svn',
+    'svn+http',
+    'svn+https',
+    'svn+ssh',
+    'bzr+http',
+    'bzr+https',
+    'bzr+ssh',
+    'bzr+sftp',
+    'bzr+ftp',
+    'bzr+lp',
+]
