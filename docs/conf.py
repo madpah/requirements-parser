294959ef80ad81796c4135ef7ac961c3e30c9194
David Fischer <djfische@gmail.com>
2015-05-02 18:27:52 -0700

    Update the year

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Requirements Parser'
-copyright = '2013, David Fischer'
+copyright = '2015, David Fischer'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
